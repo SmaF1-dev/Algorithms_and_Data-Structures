cfb51bc11eb544f78873bdd1decf8a8e29c25d52
L1tDeV <113245394+SmaF1-dev@users.noreply.github.com>
2024-09-15 23:53:15 +0300

    Add files via upload

--- lab0/1/1.py
+++ lab0/1/1.py
@@ -1,2 +1,5 @@
 a,b = map(int, input().split())
-print(a+b)
+if -10**9<=a<=10**9 and -10**9<=b<=10**9:
+    print(a+b)
+else:
+    print("Числа должны быть от -10**9 до 10**9!")
--- lab0/1/2.py
+++ lab0/1/2.py
@@ -1,2 +1,5 @@
 a,b = map(int, input().split())
-print(a+b**2)
+if -10**9<=a<=10**9 and -10**9<=b<=10**9:
+    print(a+b**2)
+else:
+    print("Числа должны быть от -10**9 до 10**9!")
