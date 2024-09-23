40fe4d37211cefaab2fb61270ab88b128d9c6aa0
L1tDeV <113245394+SmaF1-dev@users.noreply.github.com>
2024-09-23 18:12:16 +0300

    Delete lab1 directory

--- lab1/task3-var/gen_input.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt", 'w') as f:
-    n=10**3
-    f.write(str(n)+ '\n')
-    lst = []
-    st = 10**9-n+1
-    for i in range(n):
-        lst.append(st)
-        st+=1
-    f.write(' '.join(list(map(str, lst))))
--- lab1/task8-var/gen_input.py
+++
@@ -1,9 +0,0 @@
-with open("input.txt", 'w') as f:
-    n=10**3*5
-    f.write(str(n)+ '\n')
-    lst = []
-    st = 10**9
-    for i in range(n):
-        lst.append(st)
-        st-=1
-    f.write(' '.join(list(map(str, lst))))
