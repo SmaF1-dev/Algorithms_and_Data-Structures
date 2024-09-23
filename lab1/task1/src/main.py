e276c4c08f89fe3c2b92f8dcc0b5aeec523f0fd7
L1tDeV <113245394+SmaF1-dev@users.noreply.github.com>
2024-09-23 18:13:06 +0300

    Add files via upload

---
+++ lab1/task1/src/main.py
@@ -0,0 +1,34 @@
+import time
+
+def insertion_sort(n, lst):
+    for i in range(1,n):
+        elem = lst[i]
+        j=i-1
+        while j>=0 and lst[j]>elem:
+            lst[j+1]=lst[j]
+            j-=1
+        lst[j+1]=elem
+
+    return lst
+
+time_st = time.perf_counter()
+
+file_inp = open("input.txt")
+n = int(file_inp.readline())
+lst = list(map(int, file_inp.readline().split()))
+file_inp.close()
+
+if len(lst)!= n:
+    quit("Указанная длина списка не соответствует действительной!")
+
+if n>1000 or n<1 or max(lst)>10**9:
+    quit("Входные данные не соответствуют условию задачи!")
+
+lst = list(map(str, insertion_sort(n, lst)))
+out = " ".join(lst)
+
+file_out = open("output.txt", 'w')
+file_out.write(out)
+file_out.close()
+
+print(f'Время работы программы %s секунд.' % (time.perf_counter()-time_st))
---
+++ lab1/task5/src/main.py
@@ -0,0 +1,37 @@
+import time
+
+def selection_sort(n, lst):
+    for i in range(n-1):
+        m=10000000000
+        ind = -1
+        for j in range(i,n):
+            if lst[j]<m:
+                m=lst[j]
+                ind=j
+        lst[i], lst[ind] = lst[ind], lst[i]
+    return lst
+
+            
+
+time_st = time.perf_counter()
+
+file_inp = open("input.txt")
+n = int(file_inp.readline())
+lst = list(map(int, file_inp.readline().split()))
+file_inp.close()
+
+if len(lst)!= n:
+    quit("Указанная длина списка не соответствует действительной!")
+
+if n>1000 or n<1 or max(lst)>10**9:
+    quit("Входные данные не соответствуют условию задачи!")
+
+lst_ans = selection_sort(n, lst)
+
+lst = ' '.join(list(map(str, selection_sort(n, lst))))
+
+file_out = open("output.txt", 'w')
+file_out.write(lst)
+file_out.close()
+
+print(f'Время работы программы %s секунд.' % (time.perf_counter()-time_st))
